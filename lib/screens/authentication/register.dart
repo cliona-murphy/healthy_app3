43f7558b295efb0f0c99c20f2a1017fe4e263aea
cliona <clionamurphycm@hotmail.com>
2021-04-18 16:51:06 +0100

    adding regex to password and email input fields in register page

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -1,4 +1,7 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:healthy_app/services/auth.dart';
 import 'package:healthy_app/shared/constants.dart';
 import 'package:healthy_app/shared/loading.dart';
@@ -51,7 +54,17 @@ class _RegisterState extends State<Register> {
                 SizedBox(height: 20.0),
                 TextFormField(
                   decoration: textInputDecoration.copyWith(hintText: "email"),
-                  validator: (val) => val.isEmpty ? "Enter an email" : null,
+                  validator: (val) {
+                    if (val.isEmpty){
+                      return "Enter an email";
+                    } else if (!val.contains(RegExp("@"))) {
+                      return "Please supply a valid email";
+                    }
+                    else {
+                      return null;
+                    }
+                  },
+                    // val.isEmpty ? "Enter an email" : null,
                     onChanged: (val) {
                       setState(() => email = val);
                     }
@@ -59,7 +72,17 @@ class _RegisterState extends State<Register> {
                 SizedBox(height: 20.0),
                 TextFormField(
                     decoration: textInputDecoration.copyWith(hintText: "password"),
-                    validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
+                    validator: (val) {
+                      if (val.length < 6) {
+                        return "Enter a password of 6 character or more";
+                      }
+                      else if (!val.contains(RegExp("[A-Z0-9]!.,{}@?/"))){
+                        return "Include a capital letter, digit and special character.";
+                      }
+                      else {
+                        return null;
+                      }
+                    },
                     obscureText: true,
                     onChanged: (val) {
                       setState(() => password = val);
@@ -85,7 +108,7 @@ class _RegisterState extends State<Register> {
                       }
                     }
                 ),
-                SizedBox(height: 12.0),
+                SizedBox(height: 12.0, width: 600),
                 Text(
                   error,
                   style: TextStyle(
