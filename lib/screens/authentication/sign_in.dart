009a08569bfa66fb6f4fe74940c5fbda0d5c23f7
cliona <clionamurphycm@hotmail.com>
2021-04-18 17:18:50 +0100

    restyling sign in page

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -21,83 +21,118 @@ class _SignInState extends State<SignIn> {
   //text field state
   String email = "";
   String password = "";
-  String error = "";
   
   @override
   Widget build(BuildContext context) {
     return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        elevation: 0.0,
-        title: Text("Sign in to Healthy App"),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("Register"),
-            onPressed: () {
-              widget.toggleView();
-            },
+      backgroundColor: Colors.grey[200],
+      //Colors.lightBlue[50],
+      // appBar: AppBar(
+      //   backgroundColor: Colors.lightBlue,
+      //   elevation: 0.0,
+      //   title: Text("Sign in to Healthy App"),
+      //   actions: <Widget>[
+      //     FlatButton.icon(
+      //       icon: Icon(Icons.person),
+      //       label: Text("Register"),
+      //       onPressed: () {
+      //         widget.toggleView();
+      //       },
+      //     ),
+      //   ],
+      // ),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top:50)),
+          Text("Welcome Back!",
+          style: TextStyle(
+            //fontFamily: 'Raleway',
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
           ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: "email"),
-                    validator: (val) => val.isEmpty ? "Enter an email" : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  }
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: "password"),
-                  obscureText: true,
-                    validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  color: Colors.black,
-                  child: Text(
-                    "Sign in",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()){
-                      setState(() => loading = true);
-                      dynamic result = await _auth.signInWithEmail(email, password);
-                      if (result == null) {
-                        setState(() {
-                        error = "could not sign in with those credentials";
-
-                        loading = false;
-                        });
-                          return Flushbar(
-                            backgroundColor: Colors.red[600],
-                            duration: Duration(seconds: 2),
-                            flushbarPosition: FlushbarPosition.TOP,
-                            title: 'Error',
-                            message: "Could not sign in with those credentials.",
-                          )..show(context);
-                      }
+          ),
+          SingleChildScrollView(
+          child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      decoration: textInputDecoration.copyWith(hintText: "email"),
+                      validator: (val) => val.isEmpty ? "Enter an email" : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
                     }
-                  }
-                ),
-              ],
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      decoration: textInputDecoration.copyWith(hintText: "password"),
+                    obscureText: true,
+                      validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }
+                  ),
+                  SizedBox(height: 30.0),
+                  Container(
+                    width: 500,
+                    height: 50,
+                    child: RaisedButton(
+                      focusColor: Colors.grey[600],
+                      color: Colors.black,
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()){
+                          setState(() => loading = true);
+                          dynamic result = await _auth.signInWithEmail(email, password);
+                          if (result == null) {
+                            setState(() {
+                            loading = false;
+                            });
+                              return Flushbar(
+                                backgroundColor: Colors.red[600],
+                                duration: Duration(seconds: 2),
+                                flushbarPosition: FlushbarPosition.TOP,
+                                title: 'Error',
+                                message: "Could not sign in with those credentials.",
+                              )..show(context);
+                          }
+                        }
+                      }
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                      child: Text(
+                        "Don't have an account yet?",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                      TextButton(
+                        child: Text("Click to sign up",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        onPressed: () {
+                          widget.toggleView();
+                        },
+                      ),
+                  ]),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
